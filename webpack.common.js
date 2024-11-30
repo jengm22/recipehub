6155d5a1d1e744f52dd384abf24fcdddd15760ce
jengm22 <mahtarrjeng@gmail.com>
2024-11-30 18:05:52 +0000

    fix

--- webpack.common.js
+++ webpack.common.js
@@ -1,3 +1,21 @@
+// const path = require('path');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// module.exports = {
+//   entry: './app.js',
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       template: './views/index.ejs',
+//       filename: 'index.html',
+//     }),
+//   ],
+//   output: {
+//     filename: '[name].bundle.js',
+//     path: path.resolve(__dirname, 'dist'),
+//     clean: true,
+//   },
+// };
+
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
@@ -13,5 +31,16 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+  },
+  resolve: {
+    extensions: ['.js', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ejs$/,
+        use: ['html-loader', 'ejs-loader'],
+      },
+    ],
   },
 };
