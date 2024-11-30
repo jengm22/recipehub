af415c15a170c571c46c4793d6cb66e89b29c6cb
jengm22 <mahtarrjeng@gmail.com>
2024-11-30 17:38:11 +0000

    fixing errors

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,11 @@
 const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
+const commonConfig = require('./webpack.common.js');
+const TerserPlugin = require('terser-webpack-plugin');
 
-module.exports = merge(common, {
+module.exports = merge(commonConfig, {
   mode: 'production',
-  devtool: 'source-map',
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
 });
