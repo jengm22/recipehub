30fe19d55bd2413485c4530dc2a1f7990897e186
jengm22 <mahtarrjeng@gmail.com>
2024-11-30 17:14:52 +0000

    adding webpack files

---
+++ webpack.common.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: 'app.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './views/index.ejs',
+      filename: 'index.html',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+};
